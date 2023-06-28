84575f8eee97f994afdb2015b6c502b00ddc737e
Eduardo Aguilar <eduardo.aguilar.ant@gmail.com>
2023-06-28 17:11:02 -0500

    Pequeña actualizacion

--- src/main/java/com/api/BateriaCaminonMinero/Repositories/TrabajadoresRepository.java
+++ src/main/java/com/api/BateriaCaminonMinero/Repositories/TrabajadoresRepository.java
@@ -4,12 +4,13 @@ import com.api.BateriaCaminonMinero.Models.TrabajadoresModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Repository
 public interface TrabajadoresRepository extends JpaRepository<TrabajadoresModel, Long> {
 
     Optional<TrabajadoresModel> findByUsername(String username);
