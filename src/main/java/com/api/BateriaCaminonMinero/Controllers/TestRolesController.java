c649fee46470a63eafbd838b0e96250ecf7e4bf1
Eduardo Aguilar <eduardo.aguilar.ant@gmail.com>
2023-06-28 15:51:51 -0500

    Permisos por rol de usuario agregados

---
+++ src/main/java/com/api/BateriaCaminonMinero/Controllers/TestRolesController.java
@@ -0,0 +1,27 @@
+package com.api.BateriaCaminonMinero.Controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRolesController {
+    @GetMapping("/accessAdmin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String accessAdmin(){
+        return "Hola, has accedido con un rol de ADMIN";
+    }
+
+    @GetMapping("/accessUser")
+    //@PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PreAuthorize("hasRole('USER')")
+    public String accessUser(){
+        return "Hola, has accedido con un rol de USER";
+    }
+
+    @GetMapping("/accessInvited")
+    @PreAuthorize("hasRole('INVITED')")
+    public String accessInvited(){
+        return "Hola, has acceddo con un rol de INVITED";
+    }
+}
--- src/main/java/com/api/BateriaCaminonMinero/Security/WebSecurityConfig.java
+++ src/main/java/com/api/BateriaCaminonMinero/Security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -21,6 +22,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
     @Autowired
     JwtUtils jwtUtils;
